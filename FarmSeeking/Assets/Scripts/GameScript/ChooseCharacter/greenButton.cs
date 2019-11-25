d7c7e4a905e041ffe305001e573a433cc87eb6b7
Farhan Muhammad <farhnmh@gmail.com>
2019-11-25 19:12:27 +0700

    Update Asset GUI - Downloading Script

---
+++ FarmSeeking/Assets/Scripts/GameScript/ChooseCharacter/greenButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class greenButton : MonoBehaviour
+{
+    private Button choose;
+    public chooseScript chosen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        choose = GetComponent<Button>();
+        choose.onClick.AddListener(choice);
+    }
+
+    void choice()
+    {
+        chosen.colour = "green";
+    }
+}
---
+++ FarmSeeking/Assets/Scripts/GameScript/moveToScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class moveToScene : MonoBehaviour
+{
+    private Button thisButton;
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisButton = GetComponent<Button>();
+        thisButton.onClick.AddListener(move);
+    }
+
+    void move()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
